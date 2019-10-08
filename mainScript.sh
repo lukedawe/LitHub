ed59c5fd0fd263222f74f4430a2ff5d2a1aad602
JamesShaw <jamesshaw@linux123.computing.dundee.ac.uk>
2019-10-08 11:29:15 +0100

    First Menu

--- mainScript.sh
+++ mainScript.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-#
-#Main Bash Script Will be here
-#
+echo "Which action would you like to perform?"
+echo "[1] Create new logged file"
+echo "[2] Open file"
+echo "[0] Exit"
+
+read -p "Enter a Number" num
+
+case &num in 
+    1)
