80408c4ac7916b67b28d13cd4b7559d9dc497550
JamesShaw <jamesshaw@linux151.computing.dundee.ac.uk>
2019-10-15 13:14:05 +0100

    Creating repository

--- mainScript.sh
+++ mainScript.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+createRepo(){
+    read -p "Please enter the name of your new repository: " Repository
+    if[ -d "$Repository" ]; then
+        mkdir $Repository
+        cd $Repository/
+        touch log.txt
+        mkdir checkedOut 
+    else
+        echo "can't create a repository with this name"
+    fi
+    
+
+
+    
+
+
+    read -p "Open/Create a new file in this repository? [y/n]" 
+}
+
 createFile(){
     #Create File
     echo "Your file will be stored in /Repository"
-    mkdir Repository
     touch Repository/"$1.txt"
-}
-
-editFile(){
-    Repository/"$1.txt"
 }
 
 while :
@@ -16,19 +30,19 @@ do
     echo
     echo " _________________________________________"
     echo "|Which action would you like to perform?  |"
-    echo "|[1] Create new logged file               |"
-    echo "|[2] Open file                            |"
+    echo "|[1] Create new repository                |"
+    echo "|[2] Create a new file                    |"
     echo "|[0] Exit                                 |"
 
     read -p "Enter a Number: " num
 
     case $num in
         1)
-        read -p "Please enter the name of your file: " filename
-        createFile filename
-        editFile filename
+        createRepo()
         ;;
         2)
+        read -p "Please enter the name of your file: " filename
+        createFile filename 
         ;;
         0)exit 0;;
         *)echo;echo "INVALID INPUT";;
